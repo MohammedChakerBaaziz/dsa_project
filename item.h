126671f7d634251bb4abb7be759f2de1645c4ced
idriss-hamadi <110414450+idriss-hamadi@users.noreply.github.com>
2022-11-11 10:38:45 +0100

    item files
    the item cpp and header

---
+++ item.h
@@ -0,0 +1,35 @@
+#ifndef item_h
+#define item_h
+
+#include <iostream>
+#include <string>
+#include <queue>
+using namespace std;
+
+
+class Item{
+    private :
+        string Name;
+        string Id;
+        int UnitPrice;
+        int Quantity;
+    
+    public:
+        Item();
+        Item(string & ,string &  ,int &,int &);        //copy constructure
+        Item(string&&,string&&,int&&,int&&);    //move constructure
+        ~Item();                                //destructure
+        string get_name();
+        string get_id();
+        int get_Unit_price();
+        int get_quantity();
+        void set_name(string );
+        void set_id(string);
+        void set_unit_price(int);
+        void set_Quantity(int);
+};
+
+
+
+
+#endif
