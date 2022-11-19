fbfef96ff4f4de8ce401d3a5c9e7c59cce89a68b
nesrine abdelhak <89046879+nessrine19@users.noreply.github.com>
2022-11-19 22:20:31 +0100

    reading from a file (need to be fixed )

--- Part 2 with BST/main.cpp
+++ Part 2 with BST/main.cpp
@@ -5,22 +5,56 @@ using namespace std;
 
 int main()
 {
- Binarysearchtree t1;
- Item item1("BABY FOOD","3",30,1000);
- Item item2("BATTERIES","2",15,6000);
- Item item3("BBQ","1",170,200);
- Item item4("BISCUITS","10",120,300);
- Item item5("BREAD","34",15,100);
+    cout<<"break point"<<endl;
+    Binarysearchtree t1;
+    /*Item item1("BABY FOOD","3",30,1000);
+    Item item2("BATTERIES","2",15,6000);
+    Item item3("BBQ","1",170,200);
+    Item item4("BISCUITS","10",120,300);
+    Item item5("BREAD","34",15,100);
+    cout<<"break point"<<endl;
+
+       p=t1.insertelement(p,item1);
+       t1.insertelement(p,item2);
+       t1.insertelement(p,item3);
+       t1.insertelement(p,item4);*/
+
     Node* p=NULL;
     t1.setroot(p);
-    p=t1.insertelement(p,item1);
-    t1.insertelement(p,item2);
-    t1.insertelement(p,item3);
-    t1.insertelement(p,item4);
-    t1.in_order(p);
-    std::cout<<" after delete"<<std::endl;
-    t1.delete_item(p,item3);
-    std::cout<<std::endl;
-    t1.in_order(p);
+    ifstream readitems("test.txt");
+
+    if (!readitems.is_open())
+        std::cout << "ERROR: File Open" << '\n';
+
+    string n;
+    string I;
+    string un;
+    string qua;
+    Item itemvect[63];
+    Item* itr=itemvect;
+
+    while (!readitems.eof()-1)
+    {
+
+        getline(readitems>>ws, n, ',');
+        getline(readitems>>ws, I, ',');
+        getline(readitems>>ws, un, ',');
+        getline(readitems>>ws,qua, '\n');
+        itr->set_all(n,I,std::stoi(un),std::stoi(qua));
+        itr++;
+    }
+    for(int i=0;i<63;i++)
+    {
+        std::cout<<i<<std::endl;
+        std::cout<<itemvect[i].get_name()<<std::endl;
+    }
+    std::cout<<itemvect[0].get_name();
+        cout<<"break point 8"<<endl;
+
+        //t1.in_order(p);
+        cout<<"break point 9 "<<endl;
+
+    readitems.close();
+
 
 }
