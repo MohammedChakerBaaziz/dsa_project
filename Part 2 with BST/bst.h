c207b5d5b80222a0a402df77784e0d8fb1a72494
nesrine abdelhak <89046879+nessrine19@users.noreply.github.com>
2022-11-20 12:27:27 +0100

    this is the version for bst head

--- Part 2 with BST/bst.h
+++ Part 2 with BST/bst.h
@@ -2,46 +2,52 @@
 #define BST_H
 #include"item.h"
 
-class Node{
+class Node
+{
+public:
     Item data;
     Node* left;
     Node* right;
     Node();
-    Node(Item &obj){
+    Node(Item &obj)
+    {
         this->data=obj;
     }
     friend class Binarysearchtree;
 };
-class Binarysearchtree{
-    private:
-        Node* root;
-        //utility function
-         Node * findMinimum( Node*ptr )const
+class Binarysearchtree
 {
-    if( ptr == nullptr )
-        return nullptr;
-    if( ptr->left == nullptr )
-        return ptr;
-    return findMinimum( ptr->left );
- }
- 	Node* Findmax(Node* r)
-	{
-		if (r == NULL)
-			return NULL;
-		else if (r->right == NULL)
-			return r;
-		else
-			return Findmax(r->right);
-	}
+private:
+    Node* root;
+    //utility function
+    Node * findMinimum( Node*ptr )const
+    {
+        if( ptr == nullptr )
+            return nullptr;
+        if( ptr->left == nullptr )
+            return ptr;
+        return findMinimum( ptr->left );
+    }
+    Node* Findmax(Node* r)
+    {
+        if (r == NULL)
+            return NULL;
+        else if (r->right == NULL)
+            return r;
+        else
+            return Findmax(r->right);
+    }
 
-    public:
-        Binarysearchtree();
-        void setroot(Node*p);
-        Node* getroot();
-        Node* insertelement(Node*,Item&);
-        Node* delete_item(Node*,Item&);
-        bool contain(Node*, Item&)const;
-        void in_order(Node*)const;
+public:
+    Binarysearchtree();
+    void setroot(Node*p);
+    Node* getroot();
+    Node* insertelement(Node*,Item&);
+    Node* delete_item(Node*,Item&);
+    bool contain(Node*, Item&)const;
+    void in_order(Node*);
+    bool isempty();
+    void insertelement(Item &);
 
 };
 #endif // BST_H
