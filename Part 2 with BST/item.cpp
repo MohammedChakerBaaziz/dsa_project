64f186d4c435a7e0e4b855356bd9d940343ead5a
nesrine abdelhak <89046879+nessrine19@users.noreply.github.com>
2022-11-21 19:35:27 +0100

    item cpp

--- Part 2 with BST/item.cpp
+++ Part 2 with BST/item.cpp
@@ -1,42 +1,52 @@
 #include "item.h"
 #include <iostream>
 #include <string>
-string Item::get_name(){
-return Name;
+string Item::get_name()
+{
+    return Name;
 };
-string Item::get_id(){
-return Id;
+string Item::get_id()
+{
+    return Id;
 };
-int Item::get_Unit_price(){
+int Item::get_Unit_price()
+{
     return UnitPrice;
 };
-int Item::get_quantity(){
+int Item::get_quantity()
+{
     return Quantity;
 };
 
-void Item::set_name(string name1){
-Name=name1;
+void Item::set_name(string name1)
+{
+    Name=name1;
 };
 
-void Item::set_id(string id1){
-Id=id1;
+void Item::set_id(string id1)
+{
+    Id=id1;
 };
 
-void Item::set_Quantity(int quanti){
-Quantity=quanti;
+void Item::set_Quantity(int quanti)
+{
+    Quantity=quanti;
 };
 
-void Item::set_unit_price(int price){
-UnitPrice=price;
+void Item::set_unit_price(int price)
+{
+    UnitPrice=price;
 };
-Item::Item(){};
-Item::Item(string &name, string &id,int & unitprice,int & quantity){
+Item::Item() {};
+Item::Item(string &name, string &id,int & unitprice,int & quantity)
+{
     Name=name;
     Id=id;
     UnitPrice=unitprice;
     Quantity=quantity;
 };
-Item::Item(string &&name, string &&id,int && unitprice,int && quantity){
+Item::Item(string &&name, string &&id,int && unitprice,int && quantity)
+{
     Name=std::move(name);
     Id=std::move(id);
     UnitPrice=std::move(unitprice);
@@ -49,7 +59,8 @@ void Item::set_all(string n,string i,int un=100,int q=0)
     set_unit_price(un);
     set_Quantity(q);
 };
-Item::~Item(){
+Item::~Item()
+{
     Name=" ";
     Id=" ";
     UnitPrice=0;
